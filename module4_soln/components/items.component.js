5adc59c7a7ce90990a6e94f586c3bda249a2c491
Nakul Srivastava <59439064+imnakul@users.noreply.github.com>
2021-12-01 17:08:02 +0530

    Add files via upload

--- module4_soln/components/items.component.js
+++ module4_soln/components/items.component.js
@@ -1,1 +1,11 @@
+(function() {
+    'use strict';
 
+    angular.module('MenuApp')
+        .component('items', {
+            templateUrl: 'templates/items.component.html',
+            bindings: {
+                items: '<'
+            }
+        });
+})();
